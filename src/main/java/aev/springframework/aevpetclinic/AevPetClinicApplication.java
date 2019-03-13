890739d18fe9d5df5c7203471d0f7767b6536c38
Aloisio Vieira <aloisio.vieira@gmail.com>
2019-03-12 21:14:07 -0300

    first commit

---
+++ src/main/java/aev/springframework/aevpetclinic/AevPetClinicApplication.java
@@ -0,0 +1,13 @@
+package aev.springframework.aevpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AevPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AevPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aev/springframework/aevpetclinic/AevPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package aev.springframework.aevpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AevPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
