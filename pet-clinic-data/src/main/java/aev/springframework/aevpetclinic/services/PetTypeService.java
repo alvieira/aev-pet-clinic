28ecccde1192f903e8bfdc546a14bddbd7097641
Aloisio Vieira <aloisio.vieira@gmail.com>
2019-07-22 22:51:02 -0300

    Added PetType service. Closes #29

---
+++ pet-clinic-data/src/main/java/aev/springframework/aevpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package aev.springframework.aevpetclinic.services;
+
+import aev.springframework.aevpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/aev/springframework/aevpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,43 @@
+package aev.springframework.aevpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import aev.springframework.aevpetclinic.model.PetType;
+import aev.springframework.aevpetclinic.services.PetTypeService;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+	@Override
+	public Set<PetType> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType object) {
+		// TODO Auto-generated method stub
+		return super.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(PetType object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+
+}
