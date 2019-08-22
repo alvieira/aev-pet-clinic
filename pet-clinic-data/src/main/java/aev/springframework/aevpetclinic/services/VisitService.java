8e3fe69a5009c2403de5551a5ce2dbb0b6c7b159
Aloisio Vieira <aloisio.vieira@gmail.com>
2019-08-21 21:17:40 -0300

    added visit map service. Closes #46

---
+++ pet-clinic-data/src/main/java/aev/springframework/aevpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package aev.springframework.aevpetclinic.services;
+
+import aev.springframework.aevpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/aev/springframework/aevpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,36 @@
+package aev.springframework.aevpetclinic.services.map;
+
+import java.util.Set;
+
+import aev.springframework.aevpetclinic.model.Visit;
+import aev.springframework.aevpetclinic.services.VisitService;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+	@Override
+	public Visit save(Visit visit) {
+
+		if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+				|| visit.getPet().getOwner().getId() == null) {
+			throw new RuntimeException("Invalid Visit");
+		}
+
+		return super.save(visit);
+	}
+
+	public Set<Visit> findAll() {
+		return super.findAll();
+	}
+
+	public Visit findById(Long id) {
+		return super.findById(id);
+	}
+
+	public void delete(Visit visit) {
+		super.delete(visit);
+	}
+
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+}
